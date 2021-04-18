3a4f3553656c53fe6a7e03ef99b082c2e77c89ed
nwu124 <55820966+nwu124@users.noreply.github.com>
2021-04-17 20:35:30 -0700

    Initial outline

---
+++ RailwayChallenge/src/Driver.java
@@ -0,0 +1,10 @@
+
+public class Driver {
+	public static void main(String [] args) {
+		// create route manager
+		
+		// create trip calculator
+		
+		// display user options
+	}
+}
---
+++ RailwayChallenge/src/TripCalculator.java
@@ -0,0 +1,42 @@
+
+public class TripCalculator {
+	public TripCalculator() {
+		
+	}
+	// public methods
+	public int [] getOutput() {
+		// calculate all outputs
+	}
+	
+	// private methods
+	private int output1() {
+		// calculate ABC
+	}
+	private int output2() {
+		// calculate AD
+	}
+	private int output3() {
+		// calculate ADC
+	}
+	private int output4() {
+		// calculate AEBCD
+	}
+	private int output5() {
+		// calculate AED
+	}
+	private int output6() {
+		// calculate CC with max 3 stops
+	}
+	private int output7() {
+		// calculate AC with 4 stops
+	}
+	private int output8() {
+		// calculate AC
+	}
+	private int output9() {
+		// calculate BB
+	}
+	private int output10() {
+		// calculate number of CC <30
+	}
+}
